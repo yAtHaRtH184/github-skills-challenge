67901206c7c4fd3ba9dd5596551b455ab7df4dab
Yatharth Kumar Rana <rana.yatharth1804@gmail.com>
2026-09-21 06:10:12 +0000

    Increase test coverage

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -68,3 +68,12 @@ def test_get_nth_fibonacci_ten():
 
     # Assert
     assert result == 55
+
+def test_area_of_circle_negative_radius():
+    with pytest.raises(ValueError):
+        area_of_circle(-1)
+
+
+def test_get_nth_fibonacci_negative():
+    with pytest.raises(ValueError):
+        get_nth_fibonacci(-1)
